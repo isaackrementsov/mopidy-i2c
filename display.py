8506f210e4eddad3141894d6d6426cf4f5bf8eab
Isaac Krementsov <isaackrementsov@gmail.com>
2021-04-17 14:50:22 -0400

    Added comments

--- display.py
+++ display.py
@@ -3,34 +3,41 @@ import busio
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
 
-# I2C display address
+# I2C display address, change
 ADDRESS = 0x21
 
-# Display dimensions
+# Display dimensions (px)
 W = 128
 H = 32
 
+# Connect to display via I2C
 i2c = busio.I2C(board.SCL, board.SDA)
 oled = adafruit_ssd1306.SSD1306_I2C(W, H, i2c, addr=ADDRESS)
 
+# Initialize displayed image & font
 image = Image.new('1', (oled.width, oled.height))
 draw = ImageDraw.Draw(image)
 font = ImageFont.load_default()
 
+# Clear display
 def clear():
     oled.fill(0)
     oled.show()
 
+# Draw text on display
 def draw_text(text):
+    # Clear whatever was already on the screen
     clear()
 
+    # Align text horizontally left (with padding) and vertically center
     (fw, _fh) = font.getsize(text)
     draw.text(
-        (oled.width // 4, oled.height // 2 - fh // 2),
+        (oled.width // 8, oled.height // 2 - fh // 2),
         text,
         font=font,
         fill=255
     )
 
+    # Show the text on the display
     oled.image(image)
     oled.show()
--- main.py
+++ main.py
@@ -2,25 +2,34 @@ import asyncio
 from display import draw_text
 from mopidy_async_client import MopidyClient
 
-display = I2C_LCD_Driver.lcd()
-
+# Display track & artists on I2C OLED
 async def display_track(data):
     tltrack = data['tl_track']
     track = tltrack.track
 
     draw_text('Playing ' + track.name + ' by ' + ', '.join([a.name for a in track.artists]))
 
+# Initialize Mopidy listener
 async def main():
-    mopidy = await MopidyClient(parse_results=True).connect()
-    mopidy.listener.bind('track_playback_started', display_track)
-
     try:
-        while True:
-            await asyncio.sleep(0.2)
+        # Connect to Mopidy server
+        mopidy = await MopidyClient(parse_results=True).connect()
+        # Bind display_track to playback start events
+        mopidy.listener.bind('track_playback_started', display_track)
+
+        # Run program loop until KeyboardInterrupt or other exception causes an exit
+        try:
+            while True:
+                await asyncio.sleep(0.2)
+        except Exception:
+            pass
+
+        # Disconnect from Mopidy server
+        await mopidy.disconnect()
+
+    # Handle failure to connect
     except Exception:
-        pass
+        print("Failed to connect to Mopidy server. Run `sudo systemctl status mopidy` to check whether it's actually running")
 
-    await mopidy.disconnect()
-
-
+# Run async event loop
 asyncio.run(main())
