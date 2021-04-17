8de711bebbc7a99db98ddcebd9423874fc44fb41
Isaac Krementsov <isaackrementsov@gmail.com>
2021-04-16 22:23:21 -0400

    Added driver code

--- display_track.py
+++ display_track.py
@@ -1,13 +1,15 @@
 import asyncio
-
+import I2C_LCD_Driver
 from mopidy_async_client import MopidyClient
 
+display = I2C_LCD_Driver.lcd()
 
 async def display_track(data):
     tltrack = data['tl_track']
     track = tltrack.track
-    print('Name:', track.name)
-    print('Artists:', ', '.join([a.name for a in track.artists]))
+    
+    display.lcd_clear()
+    display.lcd_display_string('Playing ' + track.name + ' by ' + ', '.join([a.name for a in track.artists]))
 
 async def main():
     mopidy = await MopidyClient(parse_results=True).connect()
